101384ea51a4d363a1fb07a8fc0e13a9f6a59cb5
MarioKlaue <133053443+MarioKlaue@users.noreply.github.com>
2023-05-11 18:34:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hallo MK');
+/*!
+ * Copyright(c) 2014 Jan Blaha (pofider)
+ *
+ * Simple OData server with adapters for mongodb and nedb
+ */
+
+const ODataServer = require('./lib/odataServer.js')
+
+module.exports = function (options) {
+  return new ODataServer(options)
+}
